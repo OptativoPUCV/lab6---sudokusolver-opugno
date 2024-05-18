5d3aee603c0b4f12e5663df2f3a96f6bd080e4ad
Your Name <you@example.com>
2024-05-18 01:37:52 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -83,7 +83,11 @@ int is_valid(Node* n)
    }
 
    //Valido en submatrices
-   
+   for(int i=0; i<3;i++){
+       int arreglo[10] = {0};
+       for (int j=0; j<9 ; j++){
+         int h=3*(i/3) + (j/3);
+         int k=3*(i%3) + (j%3);
 
    return 1;
 }
